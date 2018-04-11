195708cc332461e08f969299dca85fded30042ad
sharan <sharan@LAPTOP-79FATVRD>
2018-04-11 14:38:12 -0400

    chnages done

--- Maven_project.java
+++ Maven_project.java
@@ -3,5 +3,7 @@ package maven_pom;
 public class Maven_project {
 	 public static void main(String[] args) {
 	        System.out.println("Hello World!");
+	        System.out.println("Hello World!");
+	        
 	    }
 	}
